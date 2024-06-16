39d185d499c69fee28d2d40623763379bc91315e
Shuvkant <weird426344@gmail.com>
2024-06-17 01:10:46 +0545

    fixing typo of credentials

--- Server/index.js
+++ Server/index.js
@@ -1,18 +1,19 @@
 import express from 'express'
-import cars from 'cars'
+import cors from 'cors'
 import { adminRouter } from './Routes/AdminRoute.js'
+// const cors = require('cors')
 
 const app = express()
 app.use(
-  cars({
+  cors({
     origin: ['http://localhost:5173'],
-    methods:['GET', 'POST', 'PUT'],
-    Credential
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
   })
 )
 app.use(express.json())
 app.use('/auth', adminRouter)
 
-app.listen(3000, () => { 
+app.listen(3000, () => {
   console.log('Server is running so fast')
 })
