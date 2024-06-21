7daa110531627e82d4037632ee25fbf5427f7e8c
Shuvkant <weird426344@gmail.com>
2024-06-21 23:58:28 +0545

    adding the profile in dashboard

--- EmployeeMS/src/Components/Dashboard.jsx
+++ EmployeeMS/src/Components/Dashboard.jsx
@@ -70,6 +70,15 @@ const Dashboard = () => {
                   </span>
                 </Link>
               </li>
+              <li className='w-100'>
+                <Link
+                  to='/dashboard/profile'
+                  className='nav-link px-0 align-middle text-white'
+                >
+                  <i className='fs-4 bi-person ms-2'></i>
+                  <span className='ms-2 d-none d-sm-inline'>Profile</span>
+                </Link>
+              </li>
               <li className='w-100' onClick={handleLogout}>
                 <Link className='nav-link px-0 align-middle text-white'>
                   <i className='fs-4 bi-power ms-2'></i>
