e68305cebec31131cf1366905063a506d0d6119d
Shuvkant <weird426344@gmail.com>
2024-11-04 12:15:39 +0545

    adding home route

--- EmployeeMS/src/App.jsx
+++ EmployeeMS/src/App.jsx
@@ -1,46 +1,49 @@
-import './App.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import Login from './Components/Login'
-import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom'
-import Dashboard from './Components/Dashboard'
-import Home from './Components/Home'
-import Employee from './Components/Employee'
-import Category from './Components/Category'
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Login from "./Components/Login";
+import { BrowserRouter, Route, Routes, useNavigate } from "react-router-dom";
+import Dashboard from "./Components/Dashboard";
+import Home from "./Components/Home";
+import Employee from "./Components/Employee";
+import Category from "./Components/Category";
 // import Payroll from './Components/Payroll'
-import Payroll from './Components/Payroll'
-import Profile from './Components/Profile'
-import AddCategory from './Components/AddCategory'
-import AddEmployee from './Components/AddEmployee'
-import EditEmployee from './Components/EditEmployee'
+import Payroll from "./Components/Payroll";
+import Profile from "./Components/Profile";
+import AddCategory from "./Components/AddCategory";
+import AddEmployee from "./Components/AddEmployee";
+import EditEmployee from "./Components/EditEmployee";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         {/* <Route path='/' element={<Start />}></Route> */}
-        <Route path='/adminlogin' element={<Login />}></Route>
-        <Route path='/dashboard' element={<Dashboard />}>
-          <Route path='' element={<Home />}></Route>
-          <Route path='/dashboard/employee' element={<Employee />}></Route>
-          <Route path='/dashboard/category' element={<Category />}></Route>
-          <Route path='/dashboard/Payroll' element={<Payroll />}></Route>
-          <Route path='/dashboard/profile' element={<Profile />}></Route>
+        <Route path="/" element={<Login />}></Route>
+        <Route path="/dashboard" element={<Dashboard />}>
+          <Route path="" element={<Home />}></Route>
+          <Route path="/dashboard/employee" element={<Employee />}></Route>
+          <Route path="/dashboard/category" element={<Category />}></Route>
+          <Route path="/dashboard/Payroll" element={<Payroll />}></Route>
+          <Route path="/dashboard/profile" element={<Profile />}></Route>
           <Route
-            path='/dashboard/add_category'
+            path="/dashboard/add_category"
             element={<AddCategory />}
-          ></Route>
+          >
+          </Route>
           <Route
-            path='/dashboard/add_employee'
+            path="/dashboard/add_employee"
             element={<AddEmployee />}
-          ></Route>
+          >
+          </Route>
           <Route
-            path='/dashboard/edit_employee/:id'
+            path="/dashboard/edit_employee/:id"
             element={<EditEmployee />}
-          ></Route>
+          >
+          </Route>
         </Route>
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
