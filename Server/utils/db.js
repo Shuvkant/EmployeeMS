dcae19c8fbf67c7999b2cc2519ebc4e9d6ed2522
Shuvkant <weird426344@gmail.com>
2024-11-05 14:28:59 +0545

    privateroute for unauthorized login

--- Server/utils/db.js
+++ Server/utils/db.js
@@ -1,18 +1,18 @@
-import mysql from 'mysql'
+import mysql from "mysql";
 
 const con = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'employeems',
-})
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "employeems",
+});
 
 con.connect(function (err) {
   if (err) {
-    console.log('connection error')
+    console.log("connection error");
   } else {
-    console.log('Connected')
+    console.log("Connected to the database of MySql");
   }
-})
+});
 
-export default con
+export default con;
